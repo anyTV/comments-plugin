c8694809bc8c2d0bbbbf78304a7a54da6fa294dc
wilbertverayin <wilbertverayin@gmail.com>
2015-07-29 18:08:48 +0800

    fixed multer issue

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.use(require('morgan')('combined', {stream: util.get_log_stream(config.LOGS_D
 app.use(require('method-override')());
 app.use(body_parser.urlencoded({extended: true}));
 app.use(body_parser.json());
-app.use(require('multer')({dest: config.UPLOAD_DIR}));
 app.use(require('compression')());
 
 logger.log('verbose', 'Binding custom middlewares');
