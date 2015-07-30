4d103540e75810bb9f71fe1fec6ce5cfbb9079a4
wilbertverayin <wilbertverayin@gmail.com>
2015-07-30 13:41:42 +0800

    add youtube api

--- config/router.js
+++ config/router.js
@@ -1,16 +1,27 @@
 'use strict';
 
-var importer = require('anytv-node-importer');
+var util = require(__dirname + '/../helpers/util'),
+    logger = require('anytv-node-logger'),
+    importer = require('anytv-node-importer');
 
 module.exports = function (router) {
     var c = importer.dirloadSync(__dirname + '/../controllers');
 
     router.del = router.delete;
 
+
+    router.all('*', function (req, res, next) {
+        util.log_request(req, logger);
+        next();
+    });
+
     router.get('/:topic_id', c.comments.get_comments);
     router.post('/:topic_id', c.comments.post_comments);
     router.get('/embed/:topic_id', c.comments.get_comments_view);
-    router.get('/get_comment_threads/:video_id', c.youtube.get_comment_threads);
+    router.get('/youtube/get_comment_threads', c.youtube.get_comment_threads);
+    router.get('/youtube/get_comments', c.youtube.get_comments);
+    router.get('/youtube/insert_comment_thread', c.youtube.post_comment_thread);
+    router.get('/youtube/insert_comment', c.youtube.post_comment);
 
     router.all('*', function (req, res) {
         res.status(404)
--- controllers/youtube.js
+++ controllers/youtube.js
@@ -3,18 +3,159 @@
 'use strict';
 
 var config = require(__dirname + '/../config/config'),
-    util = require(__dirname + '/../helpers/util');
+    API_BASE_URL = config.YOUTUBE.API_BASE_URL,
+    API_KEY = config.YOUTUBE.API_KEY,
+    util = require(__dirname + '/../helpers/util'),
+    superagent = require('superagent'),
+    yt_access_token = 'ya29.vwED5fFs7kPfx04JpkTWwUnfIwIT5yI0jS_qmdhq8o4Qst81YgoVP4WErJGX9C3FA2dPc2Zhvj4';
+    //req.session.youtube_chat.access_token
 
 exports.get_comment_threads = function (req, res, next) {
-    var data = util.get_data(['video_id'], [], req.params),
+    var params = util.get_data(['video_id'], ['next_page_token'], req.query),
+        query = {
+            part: 'snippet',
+            textFormat: 'plainText',
+            key: API_KEY,
+            videoId: params.video_id,
+            pageToken: (params.next_page_token || '')
+        },
 
         start = function () {
-            send_response();
+            console.log(query);
+            superagent.get(API_BASE_URL + '/commentThreads')
+                .set('Content-Type', 'application/json')
+                .set('Accept', 'application/json')
+                .query(query)
+                .send()
+                .end(send_response);
         },
 
         send_response = function (err, result) {
+            if (err) {
+                return res.send(err);
+            }
 
-            res.send(data);
+            return res.send(result.body);
+        };
+
+    start();
+};
+
+exports.get_comments = function (req, res, next) {
+    var params = util.get_data(['video_id', 'channel_id', 'parent_id'], [], req.query),
+        query = {
+            part: 'snippet',
+            textFormat: 'plainText',
+            key: API_KEY,
+            videoId: params.video_id,
+            channelId: params.channel_id,
+            parentId: params.parent_id,
+        },
+
+        start = function () {
+            superagent.get(API_BASE_URL + '/comments')
+                .set('Content-Type', 'application/json')
+                .set('Accept', 'application/json')
+                .query(query)
+                .send()
+                .end(send_response);
+        },
+
+        send_response = function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+
+            res.send(result.body);
+        };
+
+    start();
+};
+
+exports.post_comment_thread = function (req, res, next) {
+    var params = util.get_data(['video_id', 'channel_id', 'comment_text'], [], req.query),
+        data = {
+            snippet: {
+                videoId: params.video_id,
+                channelId: params.channel_id,
+                topLevelComment: {
+                    snippet: {
+                        textOriginal: params.comment_text
+                    }
+                }
+            }
+        },
+        response = {
+            endpoint: 'insert comment thread'
+        },
+
+        start = function () {
+            // if (!req.session || !req.session.youtube_chat) {
+            //     response.error = 'Not logged in';
+            //     return send_response(null, response);
+            // }
+
+            superagent.post(API_BASE_URL + '/commentThreads')
+                .set('Authorization', 'Bearer ' + yt_access_token)
+                .set('Content-Type', 'application/json')
+                .set('Accept', 'application/json')
+                .query({
+                    part: 'snippet',
+                    shareOnGooglePlus: false,
+                    alt: 'json',
+                })
+                .send(data)
+                .end(send_response);
+        },
+
+        send_response = function (err, result) {
+            if (err) {
+                response.error = err;
+            }
+
+            res.send(response);
+        };
+
+    start();
+};
+
+exports.post_comment = function (req, res, next) {
+    var params = util.get_data(['parent_id', 'comment_text'], [], req.query),
+        data = {
+            snippet:{
+                parentId: params.parent_id,
+                textOriginal: params.comment_text
+            }
+        },
+        response = {
+            endpoint: 'insert comment'
+        },
+
+        start = function () {
+            // if (!req.session || !req.session.youtube_chat) {
+            //     response.error = 'Not logged in';
+            //     return send_response(null, response);
+            // }
+
+            superagent.post(API_BASE_URL + '/comments')
+                .set('Authorization', 'Bearer ' + yt_access_token)
+                .set('Content-Type', 'application/json')
+                .set('Accept', 'application/json')
+                .query({
+                    part: 'snippet',
+                    shareOnGooglePlus: false,
+                    alt: 'json',
+                })
+                .send(data)
+                .end(send_response);
+        },
+
+        send_response = function (err, result) {
+            if (err) {
+                response.error = err;
+            }
+
+            res.send(response);
         };
 
     start();
