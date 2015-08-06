fa478ff026a2aaeb7cf592243c0ad87d7b16f925
adin234 <adinbautista@gmail.com>
2015-08-06 09:18:12 +0800

    refactored comments

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,42 @@
 'use strict';
 
+var APP_BASE_URL = 'http://dev.gamers.tm',
+    APP_PORT = 9090,
+    to_param = function (params) {
+        return Object.keys(params)
+            .map(function(param) {
+                return encodeURIComponent(param) + '=' + encodeURIComponent(params[param]);
+            })
+            .join('&');
+    };
+
 module.exports = {
     ENV: 'development',
-    PORT: 9090
+    APP_BASE_URL: APP_BASE_URL + ':' + APP_PORT,
+    PORT: APP_PORT,
+
+    YOUTUBE: {
+        BASE_URL: 'https://accounts.google.com',
+        API_BASE_URL: 'https://www.googleapis.com/youtube/v3',
+        PORT: '443',
+        AUTH_URL: '/o/oauth2/auth',
+        CHANNEL_URL: '/channels',
+        API_KEY: 'AIzaSyAP14m25_1uScfmZObKqRI4lCwveb9E8Vk',
+        client_id: '556525497714-ci74k99ts0ar37fo5pv0b5ea28es2reg.apps.googleusercontent.com',
+        client_secret: 'RDqts1yS-MMG2fREcOWIzZgk',
+        chat_redirect_uri: APP_BASE_URL + ':' + APP_PORT + '/youtube/chat_callback',
+        auth : function(params) {
+            return this.BASE_URL + ':'
+                + this.PORT
+                + this.AUTH_URL
+                + (params ? '?' + to_param(params) : '');
+        },
+        scopes: {
+            manage: 'https://www.googleapis.com/auth/youtube.force-ssl',
+            view: 'https://www.googleapis.com/auth/youtube.readonly',
+            upload: 'https://www.googleapis.com/auth/youtube.upload',
+            audit_details: 'https://www.googleapis.com/auth/youtubepartner-channel-audit'
+        },
+        gamerstm_youtuber_page: 'http://dev.gamers.tm:8080/youtubers/'
+    },
 };
---
+++ models/youtube.js
@@ -0,0 +1,105 @@
+'use strict';
+
+var config = require(__dirname + '/../config/config'),
+    util = require(__dirname + '/../helpers/util'),
+    logger = require('anytv-node-logger'),
+    mysql = require('anytv-node-mysql'),
+    request = require('superagent'),
+    squel = require('squel'),
+    moment = require('moment'),
+    _ = require('lodash');
+
+
+exports.get_comments = function (video_id, token, next) {
+    var query = {
+            part: 'snippet',
+            textFormat: 'plainText',
+            key: config.YOUTUBE.API_KEY,
+            videoId: video_id,
+            pageToken: next && isNaN(token) ? token : null
+        },
+
+        comment_body = {},
+        comments = [],
+
+        start = function () {
+            request.get(config.YOUTUBE.API_BASE_URL + '/commentThreads')
+                .set('Content-Type', 'application/json')
+                .set('Accept', 'application/json')
+                .query(query)
+                .send()
+                .end(format_data);
+        },
+
+        format_data = function (err, result) {
+            if (err) {
+                return next(err);
+            }
+
+            result = result.body;
+
+             _(result.items).forEach(function (comment) {
+                comment_body = comment.snippet.topLevelComment.snippet;
+                comments.push({
+                    email: 'empty',
+                    type: 'gamers_video',
+                    username: comment_body.authorDisplayName,
+                    topic: comment_body.videoId,
+                    comment: comment_body.textDisplay,
+                    avatar: comment_body.authorProfileImageUrl,
+                    display_date: moment(comment_body.publishedAt).fromNow(),
+                    username_link: comment_body.authorChannelUrl,
+                    reply_count: comment.snippet.totalReplyCount,
+                    channel_id: comment.snippet.channelId,
+                    comment_id: comment.id,
+                    video_id: comment.snippet.videoId
+                });
+            }).commit();
+
+
+            send_response(null, comments);
+        },
+
+        send_response = function (err, result) {
+            if (err) {
+                return next(err);
+            }
+
+            console.log(result);
+
+            return next(null, result);
+        };
+
+    start();
+};
+
+exports.get_total = function (video_id, next) {
+    var query = {
+            part: 'snippet',
+            textFormat: 'plainText',
+            key: config.YOUTUBE.API_KEY,
+            videoId: video_id,
+        },
+
+        comment_body = {},
+        comments = [],
+
+        start = function () {
+            request.get(config.YOUTUBE.API_BASE_URL + '/commentThreads')
+                .set('Content-Type', 'application/json')
+                .set('Accept', 'application/json')
+                .query(query)
+                .send()
+                .end(send_response);
+        },
+
+        send_response = function (err, result) {
+            if (err) {
+                next(err);
+            } 
+console.log(result.body);
+            next(null, [{total:result.body.pageInfo.totalResults || result.body.items.length}]);
+        };
+
+    start();
+};
